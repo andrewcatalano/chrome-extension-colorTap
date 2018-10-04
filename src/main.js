f3622efbab128a2e625fda228d614583f8eba856
neyser zana <neyser@neysers-MacBook-Pro.local>
2018-10-04 10:10:53 -0400

    draw functionality completed

--- src/main.js
+++ src/main.js
@@ -8,28 +8,52 @@ let prevX;
 let prevY;
 let currX;
 let currY;
+let flag = false;
 
-document.addEventListener('mousedown', function(e){
+canvas.addEventListener('mousedown', function(e){
     currX = e.clientX - canvas.offsetLeft;
     currY = e.clientY - canvas.offsetTop;
     draw('down', e);
 });
 
-document.addEventListener('mousemove', function(e){
-    currX = e.clientX - canvas.offsetLeft;
-    currY = e.clientY - canvas.offsetTop;
-    draw('down', e);
-})
+canvas.addEventListener('mousemove', function(e){
+    draw('move', e);
+});
 
-function draw(status, color){
+canvas.addEventListener('mouseup', function(e){
+    draw('up', e);
+});
+
+canvas.addEventListener('mouseout', function(e){
+    draw('out', e);
+});
+
+function draw(status, e){
     if (status === 'down'){
+        flag = true;
         ctx.beginPath();
         ctx.fillStyle = color;
-        ctx.fillRect(currX, currY, 10, 10);
+        ctx.fillRect(currX, currY, strokeWidth, strokeWidth);
         ctx.closePath();
     }
     if(status === 'move'){
+       if(flag === true){
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
 
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = strokeWidth;
+        ctx.stroke();
+        ctx.closePath();
+       }
+    }
+    if(status === 'up' || status === 'out'){
+        flag = false;
     }
 }
 
